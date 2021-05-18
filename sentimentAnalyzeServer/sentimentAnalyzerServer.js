1ba255a9017b21ae28548d0456f54b6a1229a1ce
sherifdd12 <you@example.comsherifdd@gmail.com>
2021-05-18 19:17:20 +0000

    Temporary changes

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,4 +1,21 @@
 const express = require('express');
+//First Added Code
+const dotenv = require('dotenv');
+dotenv.config();
+function getNLUInstance(){
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+  version: '2020-08-01',
+  authenticator: new IamAuthenticator({
+    apikey: '{apikey}',
+  }),
+  serviceUrl: '{url}',
+});
+//end of first code
 const app = new express();
 
 app.use(express.static('client'))
