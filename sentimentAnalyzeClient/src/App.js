5ba19b4f94969a093bdc17a6a4b026ec61935b71
lavskillup <73978832+lavskillup@users.noreply.github.com>
2021-08-30 18:08:24 +0530

    Update App.js

--- sentimentAnalyzeClient/src/App.js
+++ sentimentAnalyzeClient/src/App.js
@@ -45,7 +45,7 @@ class App extends React.Component {
 
     fetch(url).then((response)=>{
         response.json().then((data)=>{
-        this.setState({sentimentOutput:data});
+        this.setState({sentimentOutput:data.label});
         let output = data.label;
         let color = "white"
         switch(output) {
