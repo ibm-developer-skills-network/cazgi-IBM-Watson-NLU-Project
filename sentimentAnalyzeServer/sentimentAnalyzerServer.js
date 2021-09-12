3fae701a3cd72a301df073222612d8d23fbb7c5c
Pavan Kumar <tizzicboy@Pavans-MacBook-Pro.local>
2021-09-12 17:40:35 -0400

    add dotenv package, add code to return natural language understanding instance

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -12,16 +12,26 @@ app.use(cors_app());
 /*Uncomment the following lines to loan the environment 
 variables that you set up in the .env file*/
 
-// const dotenv = require('dotenv');
-// dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
-// const api_key = process.env.API_KEY;
-// const api_url = process.env.API_URL;
+
 
 function getNLUInstance() {
     /*Type the code to create the NLU instance and return it.
     You can refer to the image in the instructions document
     to do the same.*/
+    const api_key = process.env.API_KEY;
+    const api_url = process.env.API_URL;
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2021-08-01',
+        authenticator: new IamAuthenticator({
+          apikey: '{api_key}',
+        }),
+        serviceUrl: '{api_url}',
+    });
+    return naturalLanguageUnderstanding;
 }
 
 
